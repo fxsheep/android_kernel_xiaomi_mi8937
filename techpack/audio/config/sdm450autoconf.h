Nick89786/android_kernel_xiaomi_msm8917

techpack: Enable AW87319 codec for rolex
Signed-off-by: SunnyRaj84348 <sunnyraj7461@gmail.com>
Signed-off-by: Nick <nikhilnick00007@gmail.com>

techpack/audio/config/sdm450autoconf.h
@@ -54,3 +54,6 @@
 #define CONFIG_SND_SOC_MSM_HDMI_CODEC_RX 1
 #define CONFIG_COMMON_CLK 1
 #define CONFIG_WCD_DSP_GLINK 1
+#ifdef CONFIG_MACH_XIAOMI_ROLEX
+#define CONFIG_SND_SOC_AW87319 1
+#endif

fxsheep/android_kernel_xiaomi_mi8937

Signed-off-by: Thagoo <lohitgowda56@gmail.com>

techpack/audio/config/sdm450autoconf.h
@@ -54,3 +54,6 @@
 #define CONFIG_SND_SOC_MSM_HDMI_CODEC_RX 1
 #define CONFIG_COMMON_CLK 1
 #define CONFIG_WCD_DSP_GLINK 1
+#ifdef CONFIG_MACH_XIAOMI_ROVA
+#define CONFIG_SND_SOC_AW87319 1
+#endif